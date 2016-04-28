a8e20bfed154dcd2cd866cb822c474b095e98b13
Stan_Yx <934680697@qq.com>
2016-04-29 01:51:03 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include"Snake.h"
+#include <stdio.h>
 int main()
 {
+
 	system("MODE con: COLS=45 LINES=45");
 	Snake * ps = new Snake();
 	Wall * pw = new Wall();
 	Food * pf = new Food(*ps);
-	while (ps->checklive() && !KEYDOWN(VK_ESCAPE))
+	while (!ps->checkwall() && !KEYDOWN(VK_ESCAPE) && !ps->checkbody())
 	{
-		pw->renew();
+		Sleep(50);
+		system("cls");
+		pw->show(&pf);
+		ps->draw();
 		ps->move();
 		ps->eat(&pf);
-		ps->checkwall();
-		ps->draw();
+				
 	}
-	
-	
-	//system("cls");
-	//printf(" Game Over");
+	system("cls");
+	printf(" Game Over");
 	system("pause");
 	return 0;
 }
